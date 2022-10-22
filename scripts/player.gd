a98b5b46f71fdb940600ccda8fcb2d904e7e39d8
Zebedee-balistique <esteban.meleroz@gmail.com>
2022-10-22 17:46:36 +0200

    personnage gravité

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,66 +1,23 @@
 extends KinematicBody2D
 
-
+const GRAVITY = 50.0
+const WALK_SPEED = 200
 
 var velocity = Vector2()
 
-func get_input():
-	# Detect up/down/left/right keystate and only move when pressed.
-	velocity = Vector2()
-	if Input.is_action_pressed('move_right'):
-		velocity.x += 1
-	if Input.is_action_pressed('move_left'):
-		velocity.x -= 1
-	velocity = velocity.normalized() * speed
-
 func _physics_process(delta):
-	get_input()
-	move_and_collide(velocity * delta)
+	velocity.y += delta * GRAVITY
 
-signal hit
-
-export var speed = 400 # How fast the player will move (pixels/sec).
-var screen_size # Size of the game window.
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	screen_size = get_viewport_rect().size
-
-func _process(delta):
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("move-right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move-left"):
-		velocity.x += -1
-
-	if velocity.length() > 0:
-		velocity = velocity.normalized()*speed
-		$AnimatedSprite.play()
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -WALK_SPEED
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x =  WALK_SPEED
 	else:
-		$AnimatedSprite.stop()
-	position += velocity*delta
+		velocity.x = 0
+
+	# We don't need to multiply velocity by delta because "move_and_slide" already takes delta time into account.
+
+	# The second parameter of "move_and_slide" is the normal pointing up.
+	# In the case of a 2D platformer, in Godot, upward is negative y, which translates to -1 as a normal.
+	move_and_slide(velocity)
 	
-	if velocity.x != 0:
-		$AnimatedSprite.animation = "walk"
-		$AnimatedSprite.flip_v = false
-		$AnimatedSprite.flip_h = velocity.x < 0
-		
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-func _on_Player_body_entered(body):
-	hide()
-	emit_signal("hit")
-	$CollisionShape2D.set_deffered("disabled", true)
-
-func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
-
-
