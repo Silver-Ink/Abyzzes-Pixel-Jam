4ecd5a60b8374a0b77cebab5e587f028afaa0143
Silver-Ink <martin.74@orange.fr>
2022-10-23 15:17:44 +0200

    poisson type

--- scripts/path.gd
+++ scripts/path.gd
@@ -1,12 +1,29 @@
 extends Path2D
 
+enum itineraire{
+	a,
+	b,
+	c,
+	d,
+	e
+}
+
+enum poisson{
+	bleu,
+	rouge,
+	lanterne
+}
+
+export(itineraire) var forme
+export(poisson) var poisson_type
+
 
 onready var follow = get_node("follow")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$follow/fish.set_type(poisson_type)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
